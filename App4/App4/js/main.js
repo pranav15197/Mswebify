b848cf34593b96a3b9b9244ef4acd9ff35466a7f
Pranav <pranav15197@gmail.com>
2014-06-17 20:33:15 +0530

    initial prototype

---
+++ App4/App4/js/main.js
@@ -0,0 +1,18 @@
+var canvas = new fabric.Canvas('c1');
+function addcircle(r) {
+    canvas.add(new fabric.Circle({ radius: r, fill: '#f55', top: 100, left: 100 }));
+    canvas.selectionColor = 'rgba(0,255,0,0.3)';
+    canvas.selectionBorderColor = 'red';
+    canvas.selectionLineWidth = 5;
+}
+function addimage(url) {
+    fabric.Image.fromURL(url, function (oImg) {
+        canvas.add(oImg);
+    });
+}
+function addtext(t) {
+    var comicSansText = new fabric.IText(t, {
+        fontFamily: 'Comic Sans'
+    });
+    canvas.add(comicSansText);
+}
